d12982e1d0e0ea51a35913346bc8aaca97894000
thannnn1 <nathan.nigault@viacesi.fr>
2024-02-07 16:18:20 +0100

    <add> (Nathan) progressBar et translateProcess

---
+++ EasySave/progressBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+class progressBar
+{
+    static void Main()
+    {
+        Console.WriteLine("Lancement de la barre de progression...");
+        using (ProgressBar progressBar = new ProgressBar())
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar.Update(i);
+                Thread.Sleep(70);
+            }
+        }
+        Console.WriteLine("Appuyez sur n'importe quelle touche pour quitter");
+        Console.ReadKey();
+    }
+}
+
+class ProgressBar : IDisposable
+{
+    private const int ProgressBarLength = 70;
+    private const char ProgressBarChar = '■';
+
+    public ProgressBar()
+    {
+        Console.Write("[");
+    }
+
+    public void Update(int percent)
+    {
+        int progressLength = (int)Math.Ceiling((double)percent / 100 * ProgressBarLength);
+        Console.CursorLeft = 1;
+        Console.Write(new string(ProgressBarChar, progressLength).PadRight(ProgressBarLength));
+        Console.Write($"]{percent}%", new string(' ', 5));
+        Console.CursorLeft = 0;
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine();
+    }
+}
---
+++ EasySave/translateFrEn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Resources;
+
+class translateFrEn
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choisissez la langue : \n 1. Anglais \n 2. Français ");
+        string input = Console.ReadLine();
+
+        ResourceManager resourceManager;
+        string language;
+
+        if (input == "1")
+        {
+            resourceManager = new ResourceManager("translate.Ressources.en", typeof(translateFrEn).Assembly);
+            language = "anglais";
+        }
+        else if (input == "2")
+        {
+            resourceManager = new ResourceManager("translate.Ressources.fr", typeof(translateFrEn).Assembly);
+            language = "français";
+        }
+        else
+        {
+            Console.WriteLine("Langue non valide. Choisissez '1' pour anglais ou '2' pour français.");
+            return;
+        }
+
+        string welcomeMessage = resourceManager.GetString("welcomeMessage");
+        Console.WriteLine($"{welcomeMessage}");
+    }
+}

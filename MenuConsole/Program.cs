1e8a4a0a7fe698e325d6aaf3fdc7a9363d820692
sricart <sebastien.ricart@viacesi.fr>
2024-02-08 16:38:46 +0100

    <add>(Seb): add a test interface for our final first version of the application

---
+++ MenuConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MenuConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#===========================================#");
+            Console.WriteLine("#                                           #");
+            Console.WriteLine("#           APPLICATION EASY SAVE           #");
+            Console.WriteLine("#                                           #");
+            Console.WriteLine("#===========================================#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#--------------MENU PRINCIPAL---------------#");
+            Console.WriteLine("# 1/ Voir les sauvegardes ------------------#");
+            Console.WriteLine("# 2/ Modifier une sauvegarde ---------------#");
+            Console.WriteLine("# 3/ Exécuter un/plusieurs sauvegardes -----#");
+            Console.WriteLine("# 4/ Supprimer une sauvegarde --------------#");
+            Console.WriteLine("# 5/ Changer de langue ---------------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#---------VISUALISATION SAUVEGARDE----------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#----------MODIFICATION SAUVEGARDE----------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#-----------EXECUTION SAUVEGARDE------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#----------SUPPRESSION SAUVEGARDE-----------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#-----------CHANGEMENT DE LANGUE------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#------------------ Sauvegarde modifiée: ok #");
+            Console.WriteLine("#----------------- Sauvegarde supprimée: ok #");
+            Console.WriteLine("#------------------ Sauvegarde éxécutée: ok #");
+            Console.WriteLine("#---------------------- Langue modifiée: ok #");
+
+
+
+
+
+
+
+            Console.WriteLine("#===========================================#");
+            Console.WriteLine("#                                           #");
+            Console.WriteLine("#           APPLICATION EASY SAVE           #");
+            Console.WriteLine("#                                           #");
+            Console.WriteLine("#===========================================#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#----------------MAIN MENU------------------#");
+            Console.WriteLine("# 1/ Show saves ----------------------------#");
+            Console.WriteLine("# 2/ Edit saves ----------------------------#");
+            Console.WriteLine("# 3/ Execute one/many saves ----------------#");
+            Console.WriteLine("# 4/ Delete save ---------------------------#");
+            Console.WriteLine("# 5/ Change language -----------------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#------------SAVE VISUALIZATION-------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#----------------EDIT SAVE------------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#---------------EXECUTE SAVE----------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#---------------DELETE SAVE-----------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#--------------CHANGE LANGUAGE--------------#");
+            Console.WriteLine("#-------------------------------------------#");
+            Console.WriteLine("#---------------------- Modified backup: ok #");
+            Console.WriteLine("#------------------------- Removal save: ok #");
+            Console.WriteLine("#------------------------ Save executed: ok #");
+            Console.WriteLine("#--------------------- Language changed: ok #");
+        }
+    }
+}

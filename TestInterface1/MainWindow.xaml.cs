8e0430fb875405e7f4b731f7486bbcf5ce1895bd
sricart <sebastien.ricart@viacesi.fr>
2024-02-15 11:45:51 +0100

    <add>(Seb): interface test for 2.0 version

---
+++ TestInterface1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TabControlExample
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private SolidColorBrush _gridBackground;
+
+        public SolidColorBrush GridBackground
+        {
+            get { return _gridBackground; }
+            set
+            {
+                _gridBackground = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            // Set initial theme
+            ToggleTheme(false);
+        }
+
+        private void ToggleTheme(bool isDarkMode)
+        {
+            if (isDarkMode)
+            {
+                GridBackground = new SolidColorBrush(Colors.Black);
+            }
+            else
+            {
+                GridBackground = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void ToggleDarkMode_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleTheme(true);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

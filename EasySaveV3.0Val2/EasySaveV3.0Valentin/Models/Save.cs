0f7ee9938dd6f956e106167c04a0b00e439220a1
Valou <116378179+A1oneeee@users.noreply.github.com>
2024-02-29 16:39:57 +0100

    Add files via upload

---
+++ EasySaveV3.0Val2/EasySaveV3.0Valentin/Models/Save.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * 
+ * This class is used to store the saves and to make a list of them later
+ * 
+ */
+
+namespace EasySaveV3._0.Models
+{
+    public class Save
+    {
+        public string toClient;
+        public string Name { get; set; }
+        public string FilesSource { get; set; }
+        public string FilesTarget { get; set; }
+        public Save() { }
+        public Save(string saveName, string sourcePath, string targetPath)
+        {
+            this.Name = saveName;
+            this.FilesSource = sourcePath;
+            this.FilesTarget = targetPath;
+        }
+    }
+}
---
+++ EasySaveV3.0Val2/EasySaveV3.0Valentin/Views/AddSaveView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace EasySaveV3._0.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour AddReservationView.xaml
+    /// </summary>
+    public partial class AddSaveView : Window
+    {
+        public AddSaveView()
+        {
+            InitializeComponent();
+            DataContext = Application.Current.MainWindow.DataContext;
+        }
+        private void OnBrowseButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                ValidateNames = false,
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Sélectionner ce dossier"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string folderPath = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                if (sender == browseButtonSource)
+                {
+                    this.txtSourceFile.Text = folderPath;
+                }
+                else if (sender == browseButtonTarget)
+                {
+                    this.txtDestinationFile.Text = folderPath;
+                }
+            }
+        }
+    }
+}
